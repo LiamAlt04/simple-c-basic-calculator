680cc2d9fb3a9e005d695e060bbe0dbe57d75cb4
LiamAlt04 <liamalt88@gmail.com>
2025-09-22 11:34:44 -0500

    implemented addition feature

--- calculator.c
+++ calculator.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
 int main(){
-    
+    int x, y;
+
+    printf("please give me your first number: ");
+    scanf("%d", &x);
+
+    printf("please give me your second number: ");
+    scanf("%d", &y);
+
+    int sum = x + y;
+    printf("the sum is: %d\n", sum);
+
+    return 0;
 }
